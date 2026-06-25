83ed2ed8d48d7a59cb3b83fbe7b8dbad74f6a01e
AR7711 <ashurovriad20@gmail.com>
2026-06-25 20:44:42 +0400

    Add calculator code

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Simple Calculator");
+
+while (true)
+{
+    Console.Write("Enter first number: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter operator (+, -, *, /): ");
+    string op = Console.ReadLine();
+
+    Console.Write("Enter second number: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+
+    double result = op switch
+    {
+        "+" => a + b,
+        "-" => a - b,
+        "*" => a * b,
+        "/" => b != 0 ? a / b : throw new DivideByZeroException(),
+        _ => throw new InvalidOperationException("Unknown operator")
+    };
+
+    Console.WriteLine($"Result: {result}");
+    Console.WriteLine();
+}
